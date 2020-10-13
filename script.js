9e0bed667612f8bdcc5c8e24cbe20fb2184f5b94
ThiagoSR27 <thiagof1212@gmail.com>
2020-10-13 09:55:30 -0300

    codigo nao funcionando, em testes ainda

--- script.js
+++ script.js
@@ -1,50 +1,83 @@
-var btn = document.querySelectorAll(".key li"),
-    input = document.querySelector(".input-valor"),
-    operator = document.querySelectorAll(".operator"); 
-    
-    //array para usar todas as informaçoes da calculadora
+/*-----------------------------------------CALCULADORA----------------------------------------------------------*/
+// pego os elementos li e div input
+var btn 	  = document.querySelectorAll(".key li"),
+	input 	  = document.querySelector(".input-valor"),
+	operador  = document.querySelectorAll(".operator");
 
-    for(var i = 0; i< btn.length; i++){
-        var key = event.keyCode;
-        
-        for(var e = 0; e <= 10; e++){
-            if(key === (48+e)){
-                input.innerHTML += e;
-            }
-        }
-        switch(key){
-            case 42:
-                input.innerHTML += "*";
-                break;
-            case 43:
-                input.innerHTML += "+";
-                break;
-            case 45:
-                input.innerHTML += "-";
-                break;
-            case 46:
-                input.innerHTML += ".";
-                break;
-            case 47:
-                input.innerHTML += "/";
-                break
-            case 13 :
-            case 61:
-                var equacao = input.innerHTML;
-                if(equacao){
-                    try{
-                        input.innerHTML=eval(equacao);
-                    }
-                    catch (e){
-                        alert('Erro na expressão');
-                    }
-                }
-                break;
-            case 67:
-            case 99:
-                input.innerHTML="";
-                break;
-            default:
-                break;    
-        }
-    };
+// Percorro o array para usar todas as informações
+for(var i = 0; i < btn.length; i++){
+	document.onkeypress = function(){
+		var key = event.keyCode;
+		//console.log(key);
+		for(var e = 0; e <= 10; e++){
+			if(key === (48+e)){
+				input.innerHTML += e;
+			}
+		}
+		switch (key){
+			case 42:
+				input.innerHTML += "*";
+				break;
+			case 43:
+				input.innerHTML += "+";
+				break;
+			case 45:
+				input.innerHTML += "-";
+				break;
+			case 46:
+				input.innerHTML += ".";
+				break;
+			case 47:
+				input.innerHTML += "/";
+				break;
+			case 13:
+			case 61:
+				var equacao = input.innerHTML;
+				if(equacao){
+					try {
+						input.innerHTML = eval(equacao);
+					} catch (e) {
+						alert('Erro na expressão');
+
+					} 
+				}
+				break;
+			case 67:
+			case 99:
+				input.innerHTML = "";
+				break;						
+			default:
+				//console.log("Procurando erros?");
+				break;
+		}
+	};
+	btn[i].addEventListener('click',function(){
+		var btnVal 	 = this.innerHTML,
+			inputVal = input.innerHTML;
+		//console.log(btnVal);
+
+		// crio um clear caso o c for clicado
+		switch (btnVal){
+			case "c":
+				input.innerHTML = "";
+				break;
+			case "=":
+				// Crio a variável de equação aqui eu utilizo a função eval do javascript
+				var equacao = inputVal;
+				
+				if(equacao){
+					try {
+						input.innerHTML = eval(equacao);
+					} catch (e) {
+						alert('Erro na expressão');
+
+					} 
+				}
+				break;
+			default:
+				input.innerHTML += btnVal;
+				break;	
+		}
+
+	});
+}
